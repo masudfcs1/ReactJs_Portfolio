b4d0e3edbbc040c97f0cc00a93dca1da6af9d5dd
masudfcs1 <masudfcs1@gmail.com>
2023-05-30 10:37:35 +0600

    SocialLink use

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
-  return <div>ScoialLinks</div>;
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkdIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://linkedin.com",
+      style: "rounded-tr-md",
+    },
+
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com",
+      //   style: "rounded-tr-md",
+    },
+
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={30} />
+        </>
+      ),
+      href: "mailto:foo@gmail.com",
+      //style: "rounded-tr-md",
+    },
+
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/resume.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
+  return (
+    <div className=" flex flex-col top-[35%] left-0 fixed ">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-1000px] hover:ml-[10px] duration-300 hover:rounded-md" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className=" flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SocialLinks;
